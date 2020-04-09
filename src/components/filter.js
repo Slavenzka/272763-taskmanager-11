e35ebde934835412940021e4a79f3a28957e09a9
Ivan <i.romanov@protonmail.com>
2020-04-09 13:01:10 +0300

    Extracted filter item creation to separate function

--- src/components/filter.js
+++ src/components/filter.js
@@ -1,15 +1,23 @@
-export const createFilterTemplate = () => (
-  `<section class="main__filter filter container">
-      <input
-        type="radio"
-        id="filter__all"
-        class="filter__input visually-hidden"
-        name="filter"
-        checked
-      />
-      <label for="filter__all" class="filter__label">
-        All <span class="filter__all-count">13</span></label
-      >
+const createFilterMarkup = () => (
+  `
+    <input
+      type="radio"
+      id="filter__all"
+      class="filter__input visually-hidden"
+      name="filter"
+      checked
+    />
+    <label for="filter__all" class="filter__label">
+      All <span class="filter__all-count">13</span></label
+    >
+  `
+);
+
+export const createFilterTemplate = () => {
+  const filterMarkup = createFilterMarkup();
+  return (
+    `<section class="main__filter filter container">
+      ${filterMarkup}
       <input
         type="radio"
         id="filter__overdue"
@@ -58,4 +66,5 @@ export const createFilterTemplate = () => (
         >Archive <span class="filter__archive-count">115</span></label
       >
    </section>`
-);
+  );
+};
