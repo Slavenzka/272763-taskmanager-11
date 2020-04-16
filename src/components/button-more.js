a79158008e2217bafd4541cf16196aa9b11dbacc
Ivan <i.romanov@protonmail.com>
2020-04-16 12:09:02 +0300

    Added button load more class

--- src/components/button-more.js
+++ src/components/button-more.js
@@ -1,3 +1,26 @@
-export const createButtonMore = () => (
+import {createNode} from 'utils';
+
+const createButtonMore = () => (
   `<button class="load-more" type="button">load more</button>`
 );
+
+export default class LoadMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createButtonMore();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createNode(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
