a7aacd7bb3ca7036ca64d6e39ead78ab176e5520
Ivan <i.romanov@protonmail.com>
2020-04-19 20:43:44 +0300

    Fill abstract class with common methods

--- src/components/abstract-component.js
+++ src/components/abstract-component.js
@@ -1,7 +1,26 @@
+import {createNode} from '../utils';
+
 export default class AbstractClass {
   constructor() {
     if (new.target === AbstractClass) {
       throw new Error(`Fail on attempt to instantiate abstract class!`);
     }
+
+    this._element = null;
+  }
+
+  getTemplate() {
+    throw new Error(`Fail on attempt to call abstract method!`);
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createNode(this.getTemplate());
+    }
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
   }
 }
